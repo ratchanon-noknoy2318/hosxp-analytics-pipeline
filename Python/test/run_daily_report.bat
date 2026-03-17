1138f59fd6c7941bd1640b57c28bc362cab199e2
ratchanon2318 <ratchanon.noknoy2318@gmail.com>
2026-03-18 05:36:48 +0700

    chore: remove python folder

--- Python/test/run_daily_report.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: 1. สั่งให้ย้ายไปที่โฟลเดอร์ที่ไฟล์ .bat นี้วางอยู่ (ไม่ว่าจะเป็นที่ไหนก็ตาม)
-cd /d "%~dp0"
-
-:: 2. รันไฟล์ Python ที่อยู่ในโฟลเดอร์เดียวกัน
-python test_line_report_format.py
-python test_line_empty_data.py
-
-pause
